601748816f9ca6a18d1421201f278c99040afe66
ngrumski <grumski88@gmail.com>
2020-11-09 11:43:47 -0500

    trained new model
    model isnt trained to completion about 50% done

--- tensorflow1/models/research/object_detection/TFLite_model/saved_model/modelConverter.py
+++ tensorflow1/models/research/object_detection/TFLite_model/saved_model/modelConverter.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 # Convert the model
-converter = tf.lite.TFLiteConverter.from_saved_model("C:/Bot-On-A-Wire/tensorflow1/models/research/object_detection/TFLite_model/saved_model") # path to the SavedModel directory
+converter = tf.lite.TFLiteConverter.from_saved_model("C:/Bot-On-A-Wire/tensorflow1/models/research/object_detection/tflite_model/saved_model") # path to the SavedModel directory
 tflite_model = converter.convert()
 
 # Save the model.
