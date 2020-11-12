cc5e87294b40cd897f4a8af5b6e01561e976e074
bendraper00 <54486270+bendraper00@users.noreply.github.com>
2020-11-12 16:42:42 -0500

    Add files via upload

---
+++ Air_Cannon/debouncer.cpp
@@ -0,0 +1,46 @@
+#include "debouncer.h"
+
+db::db(int bp, unsigned long dp = 10){
+  buttonPin = bp;
+  debouncePeriod = dp;
+}
+
+void db::Init(){
+  pinMode(buttonPin, INPUT);
+}
+
+bool db::checkButtonPress(){
+  bool flag = false;
+  buttonPosition = digitalRead(buttonPin);
+  static int tempButtonPosition = buttonPosition;
+  static BUTTON_STATE statePrev = state;
+
+  switch(state){
+    case STABLE:
+    if(statePrev == UNSTABLE && buttonPosition == LOW && tempButtonPosition == HIGH){
+      tempButtonPosition = buttonPosition;
+      flag = true;
+    }
+    else if (buttonPosition != tempButtonPosition){
+      timeStart = millis();
+      tempButtonPosition = buttonPosition;
+      state = UNSTABLE;
+    }
+    break;
+
+    case UNSTABLE:
+    //Pin changes again but not enough time as elapsed -> restart timer
+    if(buttonPosition != tempButtonPosition && millis()-timeStart < debouncePeriod){
+      timeStart = millis();
+      tempButtonPosition = buttonPosition;
+      }
+    //Pin changes AND enough time elapsed -> state is stable
+    if(buttonPosition != tempButtonPosition && millis()-timeStart >= debouncePeriod){
+      statePrev = UNSTABLE;
+      state = STABLE;
+      }
+    break;
+  }
+
+  return flag;
+}
---
+++ Air_Cannon/debouncer.h
@@ -0,0 +1,21 @@
+#ifndef DEBOUNCER_H
+#define DEBOUNCER_H
+#include <Arduino.h>
+
+class db{
+  private:
+  enum BUTTON_STATE{STABLE, UNSTABLE};
+  BUTTON_STATE state = STABLE;
+  int buttonPin = -1;
+  int buttonPosition = 0;
+  unsigned long timeSince;
+  unsigned long timeStart;
+  unsigned long debouncePeriod = 10; 
+  
+  public:
+  db(int bp, unsigned long db = 10);
+  bool checkButtonPress();
+  void Init();
+};
+
+#endif
