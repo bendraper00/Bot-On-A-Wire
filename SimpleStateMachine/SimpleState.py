693b918ec86f8e8bf72d4fbd07b10fe104966b95
TrangPham68 <ttpham@wpi.edu>
2020-11-14 18:30:22 -0500

    update state machine

--- SimpleStateMachine/SimpleState.py
+++ SimpleStateMachine/SimpleState.py
@@ -10,30 +10,30 @@ class States (Enum):
     GOBACK = auto()
 
 class MyState(object):
-"""define a state object which provide some utility functions for the individual state machine"""
+#define a state object which provide some utility functions for the individual state machine
 
     def __init__(self):
         self.__state = States.DETECT
-        print "current state: ", str(self)
+        print ("current state: " + str(self.__state))
     def set_state(self,state):
         self.__state = state
     def get_state (Self, state):
         return self.__state
 
 class ControlDetection:
-    my_state = MyState()
-    stopSpeed = 1500
-    stopDistance = 30
-    speedRange = 100
-    distanceRange = 50
-    forward = True
-    detectFront = False
-    speed = 0
-    #detectBack = False
-
+    
     #Bird Detect Object in here
     def __init__(self):
+        my_state = MyState()
         my_state.set_state(States.DETECT)
+        stopSpeed = 1500
+        stopDistance = 30
+        speedRange = 100
+        distanceRange = 50
+        forward = True
+        detectFront = False
+        speed = 0
+    #detectBack = False
 
     def calcSpeed(distance, isFront): #1 for front and 0 for left
         #calculate the distance to move
@@ -61,7 +61,7 @@ class ControlDetection:
                     forward = true
                 return speed
             my_state.set_state(States.DETECT)
-            if (detect)
+            if (detectFront):
                 return calcSpeed(distance, isFront)  # if not stop then keep moving
 
         elif my_state.get_state() == States.GOBACK:
