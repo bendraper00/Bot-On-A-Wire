72af0b79bb2f00f7a779c055aec197efa1a27ee8
ngrumski <grumski88@gmail.com>
2020-11-25 17:17:34 -0500

    Update jsonCommunication.py

--- jsonCommunication.py
+++ jsonCommunication.py
@@ -25,12 +25,15 @@ if __name__ == '__main__':
         except:
             print("not usb0 or usb1")
     ser.flush()
+    out = "\n"
     ser.write(out.encode())
     while True:
-        data= { "detections": []}
+       
+        objects = []
         for i in range(15):
             if(area[i] > 0):
-                data.detections.append({"area": area[i], "xCord" : center[i][0], "yCord" :  center[i][1]})
+                objects.append({"area": area[i], "xCord" : center[i][0], "yCord" :  center[i][1]})
+        data= { "detections": objects}
         out = json.dumps(data) + "\n"
         ser.write(out.encode())
         print(out)
@@ -38,7 +41,7 @@ if __name__ == '__main__':
             line = ser.readline().decode('utf-8')
         except:
             print("An exception occurred")
-        line = line.rstrip().replace('\'', '\"').replace('\n','')
+        '''line = line.rstrip().replace('\'', '\"').replace('\n','')
         line = eval(line)
         if (line == ''):
             print("empty")
@@ -51,7 +54,7 @@ if __name__ == '__main__':
 
         hello = sensorInput["hello"]
 
-        print(str(hello) )
+        print(str(hello) )'''
         #time.sleep(1)
         #out = json.dumps({"speed": calcSpeed(front, 1)}) + "\n"
         #ser.write(out.encode())
