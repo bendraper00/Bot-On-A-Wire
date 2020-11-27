ffcd5b9b849e7ce850ff7465f170c9f6494e0b3d
ngrumski <grumski88@gmail.com>
2020-11-27 15:09:51 -0500

    Update detector.py

--- piVisionModels/detector.py
+++ piVisionModels/detector.py
@@ -9,10 +9,10 @@ import importlib.util
 
 class VideoStream:
     """Camera object that controls video streaming from the Picamera"""
-    def __init__(self,resolution=(640,480),framerate=30):
+    def __init__(self,resolution=(1080,720),framerate=30):
         # Initialize the PiCamera and the camera image stream
         self.stream = cv2.VideoCapture(0)
-        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPEG'))
+        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
         ret = self.stream.set(3,resolution[0])
         ret = self.stream.set(4,resolution[1])
             
